51a5756ab2b14139231db26aca1fd70c452460c3
Benjamin Alamu: Codeblooded <oluwaseunalamu@gmail.com>
2019-11-07 14:56:37 +0100

    Added add annoucement

--- src/components/admin/addcourse.vue
+++ src/components/admin/addcourse.vue
@@ -1,211 +1,200 @@
 <template>
-    <div>
-        <p>New Course</p>
-        <hr />
-        <div class="add-course">
-            <form>
-                <div class=fields>
-                    <label>
-                        Course Name
-                    </label>
-                    <input type="text" placeholder="e.g Principles of Programming" v-model="form.title" />
-                </div>
-                <div class=fields>
-                    <label>
-                        Course Code
-                    </label>
-                    <input type="text" placeholder="CSC 502" v-model="form.code" />
-                </div>
-                <div class=fields>
-                    <label>
-                        Units
-                    </label>
-                    <select v-model="form.units">
-                        <option value=1>1</option>
-                        <option value=2>2</option>
-                        <option value=3>3</option>
-                        <option value=4>4</option>
-                        <option value=5>5</option>
-                        <option value=6>6</option>
-                    </select>
-                    {{form.units}}
-                </div>
-                <div class=fields>
-                    <label>
-                        Lecturer
-                    </label>
-                    <select v-model="lecturer">
-                        <option v-for="(lec, index) in lecs" :value=lec._id :key="index">{{lec.name}}</option>
-                    </select>
-                    {{lecturer}}
-                </div>
-                <div class="add-btn">
-                    <button type="submit" @click.prevent="addCourse">
-                        Add Course
-                    </button>
-                    <!-- <input type="submit" @click.prevent="loginAction" /> -->
-                </div>
-            </form>
+  <div>
+    <p>New Course</p>
+    <hr />
+    <div class="add-course">
+      <form>
+        <div class="fields">
+          <label>Course Name</label>
+          <input type="text" placeholder="e.g Principles of Programming" v-model="form.title" />
         </div>
+        <div class="fields">
+          <label>Course Code</label>
+          <input type="text" placeholder="CSC 502" v-model="form.code" />
+        </div>
+        <div class="fields">
+          <label>Units</label>
+          <select v-model="form.units">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+          </select>
+          {{form.units}}
+        </div>
+        <div class="fields">
+          <label>Lecturer</label>
+          <select v-model="lecturer">
+            <option v-for="(lec, index) in lecs" :value="lec._id" :key="index">{{lec.name}}</option>
+          </select>
+          {{lecturer}}
+        </div>
+        <div class="add-btn">
+          <button type="submit" @click.prevent="addCourse">Add Course</button>
+          <!-- <input type="submit" @click.prevent="loginAction" /> -->
+        </div>
+      </form>
     </div>
+  </div>
 </template>
 
 <script>
-    import axios from "axios";
-    export default {
-        data(){
-            return{
-                addCourseResult: {},
-                assignLecturerResult: {},
-                lecs: {},
-                lecturer: null,
-                form: {
-                    title: '',
-                    code: '',
-                    units: 3,
-                }
-            }
-        },
-        mounted(){
-            this.callLecturers()
-        },
-        methods: {
-            callLecturers: async function() {
-                const res = await axios.get(
-                    "https://schoolmanagement502.herokuapp.com/api/v1/lecturer/lecturers",
+import axios from "axios";
+export default {
+  data() {
+    return {
+      addCourseResult: {},
+      assignLecturerResult: {},
+      lecs: {},
+      lecturer: null,
+      form: {
+        title: "",
+        code: "",
+        units: 3
+      }
+    };
+  },
+  mounted() {
+    this.callLecturers();
+  },
+  methods: {
+    callLecturers: async function() {
+      const res = await axios.get(
+        "https://schoolmanagement502.herokuapp.com/api/v1/lecturer/lecturers",
 
-                    {
-                        headers:{
-                            "Content-Type": 'application/json'
-                        }
-                    }
-                );
-                // console.log(res)
-                this.lecs = res.data.lecturers;//.map(i=>i.name);
-                // this.lecs = 'aa';
-
-            },
-            addCourse: async function() {
-                // console.log(res)
-                let data ={...this.form}
-                const res = await axios.post(
-                    "https://schoolmanagement502.herokuapp.com/api/v1/admin/addCourse",
-                    data, 
-                    {
-                        headers:{
-                            "Content-Type": 'application/json'
-                        }
-                    }
-                );
-
-                this.addCourseResult = res.data;
-                // this.assignLecturer(res.data);
-                console.log(this.addCourseResult)
-            },
-            assignLecturer: async function(course) {
-                // console.log(res)
-                // let lecturer = this.lecs.find(i=>i.name=this.form.lecturer)
-                const res = await axios.post(
-                    "https://schoolmanagement502.herokuapp.com/api/v1/admin/assignLecturer",
-                    {
-                        courseId: course._id,
-                        lecturerId: this.lecturer
-                    }, 
-                    {
-                        headers:{
-                            "Content-Type": 'application/json'
-                        }
-                    }
-                );
-
-                this.assignLecturerResult = res.data;
-                console.log(this.assignLecturerResult)
-            }
+        {
+          headers: {
+            "Content-Type": "application/json"
+          }
         }
+      );
+      // console.log(res)
+      this.lecs = res.data.lecturers; //.map(i=>i.name);
+      // this.lecs = 'aa';
+    },
+    addCourse: async function() {
+      // console.log(res)
+      let data = { ...this.form };
+      const res = await axios.post(
+        "https://schoolmanagement502.herokuapp.com/api/v1/admin/addCourse",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      );
+
+      this.addCourseResult = res.data;
+      // this.assignLecturer(res.data);
+      console.log(this.addCourseResult);
+    },
+    assignLecturer: async function(course) {
+      // console.log(res)
+      // let lecturer = this.lecs.find(i=>i.name=this.form.lecturer)
+      const res = await axios.post(
+        "https://schoolmanagement502.herokuapp.com/api/v1/admin/assignLecturer",
+        {
+          courseId: course._id,
+          lecturerId: this.lecturer
+        },
+        {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      );
+
+      this.assignLecturerResult = res.data;
+      console.log(this.assignLecturerResult);
     }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-    p{
-        margin-left: 2.5rem;
+p {
+  margin-left: 2.5rem;
+}
+.add-course {
+  width: 265px;
+  margin-left: 2.5rem;
+  margin-top: 2.5rem;
+  height: 500px;
+  // form{
+  //     height: auto;
+  //     display: flex;
+  //     flex-direction: column;
+  //     justify-content: space-between;
+  // }
+
+  .fields {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-top: 15px;
+
+    input {
+      width: 262px;
+      height: 40px;
+      border: 1px solid #4c52bc;
+      box-sizing: border-box;
+      border-radius: 7px;
+
+      font-family: Century Gothic;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 14px;
+      line-height: 17px;
+      letter-spacing: 0.2px;
+
+      color: rgba(0, 0, 0, 0.43);
+      padding: 5px;
     }
-    .add-course{
-        width: 265px;
-        margin-left: 2.5rem;
-        margin-top: 2.5rem;
-        height: 500px;
-        // form{
-        //     height: auto;
-        //     display: flex;
-        //     flex-direction: column;
-        //     justify-content: space-between;
-        // }
 
-        .fields{
-            display: flex;
-            flex-direction: column;
-            justify-content: space-between;
-            margin-top: 15px;
+    select {
+      width: 262px;
+      height: 40px;
+      border: 1px solid #4c52bc;
+      box-sizing: border-box;
+      border-radius: 7px;
 
-                input{
-                    width: 262px;
-                    height: 40px;
-                    border: 1px solid #4C52BC;
-                    box-sizing: border-box;
-                    border-radius: 7px;
+      font-family: Century Gothic;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 14px;
+      line-height: 17px;
+      letter-spacing: 0.2px;
 
-                    font-family: Century Gothic;
-                    font-style: normal;
-                    font-weight: normal;
-                    font-size: 14px;
-                    line-height: 17px;
-                    letter-spacing: 0.2px;
-
-                    color: rgba(0, 0, 0, 0.43);
-                    padding: 5px;
-                }
-
-                select{
-                width: 262px;
-                height: 40px;
-                border: 1px solid #4C52BC;
-                box-sizing: border-box;
-                border-radius: 7px;
-
-                font-family: Century Gothic;
-                font-style: normal;
-                font-weight: normal;
-                font-size: 14px;
-                line-height: 17px;
-                letter-spacing: 0.2px;
-
-                color: rgba(0, 0, 0, 0.43);
-                padding: 5px;
-            }
-        }
-        .add-btn{
-            margin-top: 50px;
-            display: flex;
-            justify-content: center;
-            button{
-                width: 162.5px;
-                height: 39px;
-
-                background: #4C52BC;
-                border-radius: 30px;
-
-                font-family: Century Gothic;
-                font-style: normal;
-                font-weight: normal;
-                font-size: 14px;
-                line-height: 17px;
-                /* identical to box height */
-
-                text-align: center;
-                letter-spacing: 0.142857px;
-
-                color: #FFFFFF;
-            }
-        }
+      color: rgba(0, 0, 0, 0.43);
+      padding: 5px;
     }
+  }
+  .add-btn {
+    margin-top: 50px;
+    display: flex;
+    justify-content: center;
+    button {
+      width: 162.5px;
+      height: 39px;
+
+      background: #4c52bc;
+      border-radius: 30px;
+
+      font-family: Century Gothic;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 14px;
+      line-height: 17px;
+      /* identical to box height */
+
+      text-align: center;
+      letter-spacing: 0.142857px;
+
+      color: #ffffff;
+    }
+  }
+}
 </style>
--- src/components/shared/announcements.vue
+++ src/components/shared/announcements.vue
@@ -9,9 +9,43 @@
         <p class="text-xs">{{announcement.details}}</p>
       </div>
     </div>
-    <p @click.prevent="$refs.add_new.open('add_new')" class="my-link text-sm my-4 hover:underline cursor-pointer">Create an announcement</p>
+    <p
+      @click.prevent="$refs.add_new.open('add_new')"
+      class="my-link text-sm my-4 hover:underline cursor-pointer"
+    >Create an announcement</p>
 
-    <sweet-modal ref="add_new">This is an alert.</sweet-modal>
+    <sweet-modal ref="add_new">
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full px-3 mt-4">
+          <label
+            class="block tracking-wide text-gray-700 text-left text-xs font-bold mb-2"
+            for="title"
+          >Annoucement Title</label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="title"
+            type="text"
+            v-model="form.title"
+            placeholder="e.g Upcoming Test"
+          />
+        </div>
+        <div class="w-full px-3 mt-4">
+          <label
+            class="block tracking-wide text-gray-700 text-left text-xs font-bold mb-2"
+            for="details"
+          >Annoucement Details</label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="details"
+            type="text"
+            v-model="form.details"
+            placeholder="e.g There would be CSCC 502 test next week"
+          />
+        </div>
+        <vue-element-loading v-if="loading" :active="loading" :is-full-screen="true" />
+        <button @click="addAnnoucement" class=" mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add Annoucement</button>
+      </div>
+    </sweet-modal>
   </div>
 </template>
 
@@ -25,7 +59,10 @@ export default {
   data() {
     return {
       announcements: {},
-      messageWhenNoItems: "There are not items"
+      messageWhenNoItems: "There are not items",
+      form: {},
+      user: {},
+      loading: false
     };
   },
   mounted() {
@@ -48,6 +85,32 @@ export default {
           self.error = true;
           this.error = error;
           self.$swal("Error!", "An error occured", "error");
+        });
+    },
+    addAnnoucement() {
+        const self = this;
+        this.loading = true;
+        this.user = JSON.parse(localStorage.getItem("502_user"));
+        this.form.createdBy = this.user._id
+      axios
+        .post(
+          `https://schoolmanagement502.herokuapp.com/api/v1/admin/createAnnoucement`,
+          this.form
+        )
+        .then(response => {
+          console.log(response.data);
+          this.response = response;
+          self.loading = false;
+          self.success = true;
+          self.getAnnouncements();
+          self.form = {}
+          this.$swal("Success!", "Added successfully!", "success");
+        })
+        .catch(error => {
+          self.loading = false;
+          self.error = true;
+          this.error = error;
+          self.$swal("Error!", "Invalid Credentials", "error");
         });
     }
   }
@@ -124,6 +187,49 @@ $border-color: rgba(black, 0.3);
         rgba($border-color, 0)
       )
       1 100%;
+  }
+
+  .fields {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-top: 15px;
+
+    input {
+      width: 262px;
+      height: 40px;
+      border: 1px solid #4c52bc;
+      box-sizing: border-box;
+      border-radius: 7px;
+
+      font-family: Century Gothic;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 14px;
+      line-height: 17px;
+      letter-spacing: 0.2px;
+
+      color: rgba(0, 0, 0, 0.43);
+      padding: 5px;
+    }
+
+    select {
+      width: 262px;
+      height: 40px;
+      border: 1px solid #4c52bc;
+      box-sizing: border-box;
+      border-radius: 7px;
+
+      font-family: Century Gothic;
+      font-style: normal;
+      font-weight: normal;
+      font-size: 14px;
+      line-height: 17px;
+      letter-spacing: 0.2px;
+
+      color: rgba(0, 0, 0, 0.43);
+      padding: 5px;
+    }
   }
 }
 </style>
