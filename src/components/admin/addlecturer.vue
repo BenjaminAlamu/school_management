ce0cf65489a9c1be00afe90b1c5d610d820e5024
Kekere-Ekun Fawaz Kayode <kayuskeks@gmail.com>
2019-11-07 13:01:38 +0100

    Add students/lecturers/course

--- src/components/admin/addlecturer.vue
+++ src/components/admin/addlecturer.vue
@@ -1,15 +1,153 @@
 <template>
     <div>
-        <p>Add Lecturer</p>
+        <p>New Lecturer</p>
+        <hr />
+        <div class="add-lecturer">
+            <form>
+                <div class=fields>
+                    <label>
+                        Name
+                    </label>
+                    <input type="text" placeholder="Prof Osofisan" v-model="form.name" />
+                </div>
+                <div class=fields>
+                    <label>
+                        Email
+                    </label>
+                    <input type="text" placeholder="profosofisan@gmail.com" v-model="form.email" />
+                </div>
+                <div class=fields>
+                    <label>
+                        Password
+                    </label>
+                    <input type="password" v-model="form.password" />
+                </div>
+                <div class=fields>
+                    <label>
+                        Re-enter Password
+                    </label>
+                    <input type="password" v-model="checkPassword" />
+                </div>
+                <div v-if="showMsg" class="password-error">
+                    Password does not match
+                </div>
+                <div class="add-btn">
+                    <button type="submit" @click.prevent="addLecturer">
+                        Add lecturer
+                    </button>
+                    <!-- <input type="submit" @click.prevent="loginAction" /> -->
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
 <script>
+    import axios from "axios";
     export default {
-        
+        data(){
+            return{
+                checkPassword: '',
+                showMsg: false,
+                result: {},
+                form: {
+                    name: '',
+                    email:'',
+                    passsword: '',
+                }
+            }
+        },
+        methods: {
+            addLecturer: async function() {
+                // console.log(res)
+                if((this.checkPassword===this.form.password) && (this.form.password!=='')){
+                    this.showMsg=false
+                    const res = await axios.post(
+                        "https://schoolmanagement502.herokuapp.com/api/v1/admin/registerLecturer",
+                        this.form, 
+                        {
+                            headers:{
+                                "Content-Type": 'application/json'
+                            }
+                        }
+                    );
+
+                    this.result = res;
+                } else {
+                    this.showMsg=true
+                }
+                // console.log(res)
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    p{
+        margin-left: 2.5rem;
+    }
+    .add-lecturer{
+        width: 265px;
+        margin-left: 2.5rem;
+        margin-top: 2.5rem;
+        height: 500px;
+        // form{
+        //     height: auto;
+        //     display: flex;
+        //     flex-direction: column;
+        //     justify-content: space-between;
+        // }
 
+        .fields{
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            margin-top: 15px;
+
+                input{
+                    width: 262px;
+                    height: 40px;
+                    border: 1px solid #4C52BC;
+                    box-sizing: border-box;
+                    border-radius: 7px;
+
+                    font-family: Century Gothic;
+                    font-style: normal;
+                    font-weight: normal;
+                    font-size: 14px;
+                    line-height: 17px;
+                    letter-spacing: 0.2px;
+
+                    color: rgba(0, 0, 0, 0.43);
+                    padding: 5px;
+                }
+        }
+        .password-error{
+            color: red;
+        }
+        .add-btn{
+            margin-top: 50px;
+            display: flex;
+            justify-content: center;
+            button{
+                width: 162.5px;
+                height: 39px;
+
+                background: #4C52BC;
+                border-radius: 30px;
+
+                font-family: Century Gothic;
+                font-style: normal;
+                font-weight: normal;
+                font-size: 14px;
+                line-height: 17px;
+                /* identical to box height */
+
+                text-align: center;
+                letter-spacing: 0.142857px;
+
+                color: #FFFFFF;
+            }
+        }
+    }
 </style>
