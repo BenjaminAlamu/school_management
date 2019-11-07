9e83ec9a364b6c5cd4661cb5dd19a6059fb48f85
Benjamin Alamu: Codeblooded <oluwaseunalamu@gmail.com>
2019-11-07 09:38:31 +0100

    Added content to header

--- src/components/admin/students.vue
+++ src/components/admin/students.vue
@@ -3,24 +3,24 @@
     <p class>Students</p>
     <hr class="mt-2 mr-4" />
     <table class="table-auto w-full">
-  <thead class="">
-    <tr>
-      <th class="px-4 py-2">Name</th>
-      <th class="px-4 py-2">Matric</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="(student, index) in students" :key="index" :class="{'bg-gray-100': index % 2 !== 0}">
-      <td class="border px-4 py-2">{{student.name}}</td>
-      <td class="border px-4 py-2">{{student.matric}}</td>
-    </tr>
-    <!-- <tr class="bg-gray-100">
-      <td class="border px-4 py-2">A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
-      <td class="border px-4 py-2">Adam</td>
-    </tr> -->
-  </tbody>
-</table>
-    <p ></p>
+      <thead class>
+        <tr>
+          <th class="px-4 py-2">Name</th>
+          <th class="px-4 py-2">Matric</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(student, index) in students"
+          :key="index"
+          :class="{'bg-gray-100': index % 2 !== 0}"
+        >
+          <td class="border px-4 py-2">{{student.name}}</td>
+          <td class="border px-4 py-2">{{student.matric}}</td>
+        </tr>
+      </tbody>
+    </table>
+    <p></p>
   </div>
 </template>
 
@@ -29,7 +29,7 @@ import axios from "axios";
 export default {
   data() {
     return {
-        students: {}
+      students: {}
     };
   },
   mounted() {
@@ -37,15 +37,15 @@ export default {
   },
   methods: {
     getStudents() {
-              axios
+      axios
         .get(
-          `https://schoolmanagement502.herokuapp.com/api/v1/student/students`,
+          `https://schoolmanagement502.herokuapp.com/api/v1/student/students`
         )
         .then(response => {
           console.log(response.data);
           this.students = response.data.students;
           self.loading = false;
-          self.success = true;          
+          self.success = true;
         })
         .catch(error => {
           self.loading = false;
--- src/components/shared/header.vue
+++ src/components/shared/header.vue
@@ -1,32 +1,49 @@
 <template>
-<nav class="flex items-center justify-between flex-wrap p-6">
-  <div class="flex items-center flex-shrink-0 text-white mr-6">
-    <div class=" px-3 mb-6 md:mb-0">
-      <input class="appearance-none block w-full bg-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none text-black" id="grid-first-name" type="text" placeholder="Jane">
+  <nav class="flex items-center justify-between flex-wrap p-6">
+    <div class="flex items-center flex-shrink-0 text-white mr-6">
+      <div class="px-3 mb-6 md:mb-0">
+        <input
+          class="appearance-none block w-full bg-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none text-black"
+          id="grid-first-name"
+          type="text"
+          placeholder="Jane"
+        />
+      </div>
     </div>
-  </div>
-  <div class="block lg:hidden">
-    <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-      <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-    </button>
-  </div>
-  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-    <div class="text-sm lg:flex-grow">
+    <div class="block lg:hidden">
+      <button
+        class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+      >
+        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <title>Menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+        </svg>
+      </button>
     </div>
-    <div>
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div class="text-sm lg:flex-grow"></div>
+      <div class="flex">
         <i class="mx-2 text-3xl fas fa-bell"></i>
         <i class="mx-2 text-3xl fas fa-user-circle"></i>
+        <p class="mt-1">Hello {{this.user.name.split(" ")[0]}}</p>
+      </div>
     </div>
-  </div>
-</nav>      
+  </nav>
 </template>
 
 <script>
-    export default {
-        
-    }
+// import axios from "axios";
+export default {
+  data() {
+    return {
+      user: {}
+    };
+  },
+  mounted() {
+    this.user = JSON.parse(localStorage.getItem("502_user"));
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
